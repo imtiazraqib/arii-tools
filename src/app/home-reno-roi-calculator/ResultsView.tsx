6aa5a96e1c383e921e66b2f515fb698a1d624c7b
imtiazraqib <imtiazud@ualberta.ca>
2025-03-24 10:25:20 -0600

    fixed eslint issue

--- src/app/home-reno-roi-calculator/ResultsView.tsx
+++ src/app/home-reno-roi-calculator/ResultsView.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import type { CalculationResult } from "./page";
 // import { ArrowLeft, Download, Share2 } from "lucide-react";
 
